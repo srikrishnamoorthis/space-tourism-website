78ce9fd56b7fe607427b8590d04e74b5ccbe2de6
srikrishnamoorthis <shrikichu@gmail.com>
2023-03-26 13:17:53 +0530

    Use updated path for assets.

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     return (
         <div className={classnames('min-h-screen mx-auto min-w-[375px] md:pl-8 lg:max-w-[1440px] lg:pl-14 lg:pt-8 bg-primary \
             isolate grid grid-rows-[min-content_1fr]', {
-                'lg:bg-[url(./assets/home/background-home-desktop.jpg)] md:bg-[url(./assets/home/background-home-tablet.jpg)] bg-[url(./assets/home/background-home-mobile.jpg)] bg-center bg-cover': isHomePage
+                'lg:bg-[url(/assets/home/background-home-desktop.jpg)] md:bg-[url(/assets/home/background-home-tablet.jpg)] bg-[url(/assets/home/background-home-mobile.jpg)] bg-center bg-cover': isHomePage
             })}>
             <Header />
             <Routes>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,6 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import classnames from 'classnames';
-import logo from '../assets/shared/logo.svg';
-import hamburger from '../assets/shared/icon-hamburger.svg';
-import closeIcon from '../assets/shared/icon-close.svg';
 
 function Header() {
     const [isOpen, setIsOpen] = useState(false);
@@ -13,14 +10,14 @@ function Header() {
     }
     return (
         <nav className='px-6 lg:px-0 md:p-0 md:pl-6 h-24 flex justify-between items-center'>
-            <img src={logo} alt="" className='h-12 w-12' />
+            <img src='/assets/shared/logo.svg' alt="" className='h-12 w-12' />
             {/* Line */}
             <div className='hidden lg:block h-[1px] bg-white lg:ml-12 flex-grow' />
             {/* Menu icon */}
-            <img src={hamburger} alt="" className={classnames('md:hidden', { 'hidden': isOpen, 'block': !isOpen })} onClick={toggleMenu} />
+            <img src='/assets/shared/icon-hamburger.svg' alt="" className={classnames('md:hidden', { 'hidden': isOpen, 'block': !isOpen })} onClick={toggleMenu} />
             {/* Close Icon */}
-            <img src={closeIcon} alt="" className={classnames('md:hidden relative z-[2]', { 'hidden': !isOpen, 'block': isOpen })} onClick={toggleMenu} />
-            <div className={classnames('absolute top-0 right-0 bottom-0 left-[30%] md:static md:px-16 h-full font-body \
+            <img src='/assets/shared/icon-close.svg' alt="" className={classnames('md:hidden relative z-[2]', { 'hidden': !isOpen, 'block': isOpen })} onClick={toggleMenu} />
+            <div className={classnames('absolute top-0 right-0 bottom-0 left-[30%] md:static md:px-16 h-full font-body-cond \
                tracking-widest text-secondary bg-[rgba(255,_255,_255,_0.1)] backdrop-blur-2xl md:flex md:gap-12 items-center z-[1]', {
                 'hidden': !isOpen,
                 'block': isOpen
@@ -30,28 +27,28 @@ function Header() {
                         <NavLink
                             to='/'
                             className={({ isActive }) => classnames('no-underline md:h-full md:flex md:items-center md:hover:border-b hover:border-white', { 'md:border-b-2 md:border-white': isActive })}>
-                            <><span className='pr-2 font-semibold'>00</span>HOME</>
+                            <><span className='pr-2 font-bold'>00</span>HOME</>
                         </NavLink>
                     </li>
                     <li className='px-16 pb-8 md:px-0 md:pb-0 md:h-full'>
                         <NavLink
                             to='/destination'
                             className={({ isActive }) => classnames('no-underline md:h-full md:flex md:items-center md:hover:border-b hover:border-white', { 'md:border-b-2 md:border-white': isActive })}>
-                            <><span className='pr-2 font-semibold'>03</span>DESTINATION</>
+                            <><span className='pr-2 font-bold'>03</span>DESTINATION</>
                         </NavLink>
                     </li>
                     <li className='px-16 pb-8 md:px-0 md:pb-0 md:h-full'>
                         <NavLink
                             to='/crew'
                             className={({ isActive }) => classnames('no-underline md:h-full md:flex md:items-center md:hover:border-b hover:border-white', { 'md:border-b-2 md:border-white': isActive })}>
-                            <><span className='pr-2 font-semibold'>03</span>CREW</>
+                            <><span className='pr-2 font-bold'>03</span>CREW</>
                         </NavLink>
                     </li>
                     <li className='px-16 pb-8 md:px-0 md:pb-0 md:h-full'>
                         <NavLink
                             to='/technology'
                             className={({ isActive }) => classnames('no-underline md:h-full md:flex md:items-center md:hover:border-b hover:border-white', { 'md:border-b-2 md:border-white': isActive })}>
-                            <><span className='pr-2 font-semibold'>03</span>TECHNOLOGY</>
+                            <><span className='pr-2 font-bold'>03</span>TECHNOLOGY</>
                         </NavLink>
                     </li>
                 </ul>
